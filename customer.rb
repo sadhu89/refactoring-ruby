d114c832e8be68ebaf8cfd762e93543c9d48af97
Carlos Rojas <carlos.sadhu@gmail.com>
2017-07-13 11:40:00 +1000

    Extract frequent_renter_points

--- customer.rb
+++ customer.rb
@@ -14,12 +14,7 @@ class Customer
     total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |rental|
-      # add frequent renter points
-      frequent_renter_points += 1
-      # add bonus for a two day new release rental
-      if rental.movie.price_code == Movie::NEW_RELEASE && rental.days_rented > 1
-        frequent_renter_points += 1
-      end
+      frequent_renter_points += rental.frequent_renter_points
       # show figures for this rental
       result += "\t" + rental.movie.title + "\t" + rental.charge.to_s + "\n"
       total_amount += rental.charge
